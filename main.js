f25976cdc6e86a234c1e13dec3fa56f6ffef3d3e
hectorvse <hectorvse@gmail.com>
2020-10-08 09:42:47 -0500

    version Hector

---
+++ main.js
@@ -0,0 +1,9 @@
+var usuario = document.getElementByID('usuario');
+var usub = document.getElementByID('usub');
+var contrasena = document.getElementById('contraseña')
+var clientes = document.getElementByID('clientes');
+var usuario = document.getElementByID('csub');
+
+usub.submit(function(){
+	alert("hola");
+})
